7363331e251d67393bbf567c4a032808706c7245
eckig <eckig@users.noreply.github.com>
2020-10-20 18:44:15 +0200

    Added missing file

---
+++ src/main/java/notify/GMailNotification.java
@@ -0,0 +1,15 @@
+package notify;
+
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+
+public class GMailNotification extends MailNotification
+{
+    public static final String ENV_GMAIL_USER = "GMAIL_USER";
+    public static final String ENV_GMAIL_PASSWORD = "GMAIL_PASSWORD";
+
+    public GMailNotification(String pUser, String pPassword) throws AddressException
+    {
+        super("smtp.gmail.com", 587, pUser, pPassword, new InternetAddress(pUser), new InternetAddress(pUser));
+    }
+}
